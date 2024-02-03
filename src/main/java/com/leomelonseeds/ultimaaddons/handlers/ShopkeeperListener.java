d9247b1076253e14ebb12303ee2818ff12aeaad8
xerish <xergred@gmail.com>
2024-02-03 13:05:10 -0500

    Remove redundant update call and cleanup

--- src/main/java/com/leomelonseeds/ultimaaddons/handlers/ShopkeeperListener.java
+++ src/main/java/com/leomelonseeds/ultimaaddons/handlers/ShopkeeperListener.java
@@ -7,7 +7,6 @@ import com.leomelonseeds.ultimaaddons.utils.TimeParser;
 import com.nisovin.shopkeepers.api.events.ShopkeeperOpenUIEvent;
 import com.nisovin.shopkeepers.api.events.ShopkeeperTradeCompletedEvent;
 import com.nisovin.shopkeepers.api.events.ShopkeeperTradeEvent;
-import com.nisovin.shopkeepers.api.shopobjects.entity.EntityShopObject;
 import com.nisovin.shopkeepers.api.ui.DefaultUITypes;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.trading.MerchantOffers;
@@ -68,18 +67,16 @@ public class ShopkeeperListener implements Listener {
         if (rsk.isBroken())
             return;
 
-        int size = e.getShopkeeper().getTradingRecipes(null).size();
         int index = e.getShopkeeper().getTradingRecipes(null).indexOf(e.getCompletedTrade().getTradingRecipe());
-
-        List<Integer> counts = rsk.getUses(e.getCompletedTrade().getPlayer());
+        List<Integer> uses = rsk.getUses(e.getCompletedTrade().getPlayer());
 
         // First time trade
-        if (counts.isEmpty())
-            counts = new ArrayList<>(Collections.nCopies(size, 0));
+        if (uses.isEmpty())
+            uses = new ArrayList<>(Collections.nCopies(e.getShopkeeper().getTradingRecipes(null).size(), 0));
 
-        int usage = counts.get(index) + 1;
-        counts.set(index, usage);
-        rsk.setUses(e.getCompletedTrade().getPlayer(), counts);
+        int usage = uses.get(index) + 1;
+        uses.set(index, usage);
+        rsk.setUses(e.getCompletedTrade().getPlayer(), uses);
 
         int limit = rsk.getLimits().get(index);
         if (limit - usage <= plugin.getConfigFile().limit_warn_start)
@@ -89,8 +86,6 @@ public class ShopkeeperListener implements Listener {
             String countdown = TimeParser.timeUntil(this.plugin.getConfigFile().restock_time);
             CommandUtils.sendMsg(e.getCompletedTrade().getPlayer(),
                     "&7Restock in: &b(" + countdown + ")");
-            // Update their client GUI since it's out of stock
-            updateTrades(e.getCompletedTrade().getPlayer(), rsk);
         }
         plugin.writeTradesFile();
     }
@@ -99,50 +94,48 @@ public class ShopkeeperListener implements Listener {
     public void onShopkeeperOpen(ShopkeeperOpenUIEvent e) {
         if (e.isCancelled())
             return;
+        if (e.getUIType() != DefaultUITypes.TRADING())
+            return;
+
         RotatingShopkeeper rsk = plugin.getSKLinker().getRotatingShopkeeper(e.getShopkeeper().getId());
         if (rsk == null)
             return;
         if (rsk.isBroken())
-            return;
-        if (e.getUIType() != DefaultUITypes.TRADING())
             return;
 
         updateTrades(e.getPlayer(), rsk);
     }
 
     private void updateTrades(Player p, RotatingShopkeeper rsk) {
-        Bukkit.getScheduler().runTask(plugin, () ->
-                update(p, rsk)
+        Bukkit.getScheduler().runTask(plugin, () -> {
+                    InventoryView openInventory = p.getOpenInventory();
+                    assert openInventory.getTopInventory().getType() == InventoryType.MERCHANT;
+                    MerchantInventory merchantInventory = (MerchantInventory) openInventory.getTopInventory();
+                    Merchant merchant = merchantInventory.getMerchant();
+                    List<MerchantRecipe> recipes = merchant.getRecipes();
+
+                    if (recipes.isEmpty())
+                        return;
+
+                    List<Integer> limits = rsk.getLimits();
+                    List<Integer> uses = rsk.getUses(p);
+
+                    for (int i = 0; i < recipes.size(); i++) {
+                        MerchantRecipe recipe = recipes.get(i);
+                        int limit = limits.get(i);
+                        int usage = uses.isEmpty() ? 0 : uses.get(i);
+
+                        recipe.setMaxUses(limit);
+                        recipe.setUses(usage);
+                    }
+
+                    // Send packet
+                    sendOffers(p);
+                }
         );
     }
 
-    private void update(Player p, RotatingShopkeeper rsk) {
-        InventoryView openInventory = p.getOpenInventory();
-        assert openInventory.getTopInventory().getType() == InventoryType.MERCHANT;
-        MerchantInventory merchantInventory = (MerchantInventory) openInventory.getTopInventory();
-        Merchant merchant = merchantInventory.getMerchant();
-        List<MerchantRecipe> recipes = merchant.getRecipes();
-
-        if (recipes.isEmpty())
-            return;
-
-        List<Integer> limits = rsk.getLimits();
-        List<Integer> uses = rsk.getUses(p);
-
-        for (int i = 0; i < recipes.size(); i++) {
-            MerchantRecipe recipe = recipes.get(i);
-            int limit = limits.get(i);
-            int usage = uses.isEmpty() ? 0 : uses.get(i);
-
-            recipe.setMaxUses(limit);
-            recipe.setUses(usage);
-        }
-
-        // NMS packet update
-        clientUpdate(p);
-    }
-
-    private void clientUpdate(Player player) {
+    private void sendOffers(Player player) {
         Inventory openInventory = player.getOpenInventory().getTopInventory();
         if (!(openInventory instanceof MerchantInventory merchantInventory))
             return;
@@ -160,12 +153,13 @@ public class ShopkeeperListener implements Listener {
             canRestock = true;
             merchantLevel = villager.getVillagerLevel();
             merchantExperience = villager.getVillagerExperience();
-        } else if (merchant instanceof AbstractVillager) {
+        } else if (merchant instanceof AbstractVillager)
             nmsMerchant = ((CraftAbstractVillager) merchant).getHandle();
-        } else {
+        else {
             nmsMerchant = ((CraftMerchant) merchant).getMerchant();
             merchantLevel = 0;
         }
+
         MerchantOffers merchantRecipeList = nmsMerchant.getOffers();
         if (merchantRecipeList == null)
             merchantRecipeList = new MerchantOffers();
