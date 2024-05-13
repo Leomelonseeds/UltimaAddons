25551e42efe6faff6c0bda688ad84feffe8a432c
Leomelonseeds <leonardfang15@gmail.com>
2024-05-12 23:55:05 -0700

    Fix recipe command alias

--- src/main/java/com/leomelonseeds/ultimaaddons/commands/UARecipes.java
+++ src/main/java/com/leomelonseeds/ultimaaddons/commands/UARecipes.java
@@ -1,16 +1,18 @@
 package com.leomelonseeds.ultimaaddons.commands;
 
+import org.bukkit.entity.Player;
+
+import com.leomelonseeds.ultimaaddons.UltimaAddons;
+import com.leomelonseeds.ultimaaddons.invs.RecipeList;
+import com.leomelonseeds.ultimaaddons.utils.CommandUtils;
+
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Description;
-import com.leomelonseeds.ultimaaddons.UltimaAddons;
-import com.leomelonseeds.ultimaaddons.invs.RecipeList;
-import com.leomelonseeds.ultimaaddons.utils.CommandUtils;
-import org.bukkit.entity.Player;
 
-@CommandAlias("urecipes")
+@CommandAlias("recipes")
 public class UARecipes extends BaseCommand {
     private final UltimaAddons plugin;
 
